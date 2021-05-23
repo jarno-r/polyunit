708b1d0074fbcc043b41c9f46b455b103c001042
jarno-r <jrajala35@gmail.com>
2021-05-23 20:22:24 +0200

    Update implicits.scala

--- src/main/scala/polyunit/implicits.scala
+++ src/main/scala/polyunit/implicits.scala
@@ -3,7 +3,7 @@ package polyunit
 import scala.language.implicitConversions
 
 object implicits {
-  implicit class PolyUnitConverted[A](a : A) {
+  implicit class PolyUnitConverter[A](a : A) {
     def unit : PolyUnit[A] = PolyUnit(a)
   }
 
