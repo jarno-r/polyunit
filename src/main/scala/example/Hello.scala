2fc1d35d60c9480b1e5bd38d57391127ee57d51c
Jarno Rajala <jarno.rajala@schibsted.com>
2021-05-25 16:06:38 +0300

    circus

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -25,6 +25,8 @@ object Hello {
     println(s)
     // conflict due to Monad & Traverse
     // println(s.map(i => i+1))
+
+    circus()
   }
 
   // Convert 1f and 2 to PolyUnit[Float] and PolyUnit[int] and parMapN over them to get PolyUnit[Float].
@@ -38,5 +40,25 @@ object Hello {
       i <- List(1, 2, 3).traverse(i => PolyUnit(i))
       j <- List(4, 5, 6).traverse(i => PolyUnit(i))
     } yield i++j
+  }
+
+
+  def circus(): Unit =  {
+
+    println("\n\n----- CIRCUS -----\n")
+
+    val bob1 : Sideshow[Int] = Sideshow(123)
+    val bob2 : Sideshow[Int] = Bob("Lisa")
+    val bob3 = bob1.map(i => i+456).flatMap(i => bob2.map(j => i+j))
+
+    val k1 : Clown[Int] = Krusty("Bart")
+    val k2 : Clown[Int] = Krusty("Lisa")
+
+    println(bob3)
+    println(bob1.product(bob2))
+    println(List(bob1, bob2).sequence)
+    println(List(bob1, bob2).parSequence)
+    println(k1.product(k2))
+    println((Krusty("L"):Clown[Int => (Int => Int)]).ap(k1).ap(k2))
   }
 }
---
+++ src/main/scala/polyunit/Sideshow.scala
@@ -0,0 +1,59 @@
+package polyunit
+
+import cats.arrow.FunctionK.lift
+import cats.{Applicative, Monad, Parallel, ~>}
+
+sealed trait Sideshow[+A] {}
+final case class Bob[A](target: String) extends Sideshow[A]
+
+sealed trait Clown[+A] {}
+final case class Krusty[A](audience: String) extends Clown[A]
+
+object Sideshow {
+  def apply[A](a : A): Sideshow[A] = monadSideshow.pure(a)
+
+  implicit object monadSideshow extends Monad[Sideshow] {
+    override def pure[A](x: A): Sideshow[A] = Bob("Bart")
+
+    // product(fa,fb) = fa.flatMap(a => fb.map(b => (a,b))
+    override def flatMap[A, B](fa: Sideshow[A])(f: A => Sideshow[B]): Sideshow[B] = fa match {
+      case Bob(a) => Bob(a)
+    }
+
+    override def tailRecM[A, B](a: A)(f: A => Sideshow[Either[A, B]]): Sideshow[B] =
+      flatMap(f(a)) {
+        case Left(a) => tailRecM(a)(f)
+        case Right(b) => pure(b)
+      }
+  }
+
+  implicit object parallelSideshow extends Parallel[Sideshow] {
+    override type F[A] = Clown[A]
+
+    override def applicative: Applicative[F] = polyunit.Clown.applicativeClown
+    override def monad: Monad[Sideshow] = monadSideshow
+
+    private def seq[A](krusty: F[A]): Sideshow[A] = krusty match {
+      case Krusty(a) => Bob(a)
+    }
+
+    private def par[A](bob: Sideshow[A]): Clown[A] = bob match {
+      case Bob(a) => Krusty(a)
+    }
+
+    override def sequential: F ~> Sideshow = lift(seq)
+    override def parallel: Sideshow ~> F = lift(par)
+  }
+}
+
+object Clown {
+  def apply[A](a : A): Clown[A] = applicativeClown.pure(a)
+
+  implicit object applicativeClown extends Applicative[Clown] {
+    override def pure[A](x: A): Clown[A] = Krusty("Lisa")
+
+    override def ap[A, B](ff: Clown[A => B])(fa: Clown[A]): Clown[B] = (ff, fa) match {
+      case (Krusty(a), Krusty(b)) => Krusty(a + ", " + b)
+    }
+  }
+}
