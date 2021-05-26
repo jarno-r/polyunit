ddc3f60e73125dfdd9d901426d2026ed98c2fd05
Jarno Rajala <jarno.rajala@schibsted.com>
2021-05-26 11:07:30 +0300

    Fun Tests

--- build.sbt
+++ build.sbt
@@ -9,7 +9,11 @@ lazy val root = (project in file("."))
   .settings(
     name := "polyunit",
     libraryDependencies += scalaTest % Test,
-    libraryDependencies += "org.typelevel" %% "cats-core" % "2.3.0"
+    libraryDependencies += "org.scalatestplus" %% "scalacheck-1-15" % "3.2.5.0" % Test,
+    libraryDependencies += "org.typelevel" %% "cats-core" % "2.3.0",
+    libraryDependencies += "org.typelevel" %% "cats-laws" % "2.3.0" % Test,
+    libraryDependencies += "org.typelevel" %% "discipline-scalatest" % "2.1.5" % Test,
+    libraryDependencies += "com.github.alexarchambault" %% "scalacheck-shapeless_1.14" % "1.2.3" % Test,
   )
 
 // See https://www.scala-sbt.org/1.x/docs/Using-Sonatype.html for instructions on how to publish to Sonatype.
---
+++ src/test/scala/polyunit/SideshowSpec.scala
@@ -0,0 +1,25 @@
+package polyunit
+
+
+import cats.Eq
+import cats.implicits._
+import cats.laws.discipline.MonadTests
+import org.scalacheck.{Arbitrary, Gen}
+import org.scalatest.funsuite.AnyFunSuiteLike
+import org.scalatest.matchers.should.Matchers
+import org.scalatestplus.scalacheck.ScalaCheckDrivenPropertyChecks
+import org.typelevel.discipline.scalatest.FunSuiteDiscipline
+
+class SideshowSpec extends AnyFunSuiteLike with Matchers with ScalaCheckDrivenPropertyChecks with FunSuiteDiscipline {
+
+  implicit def eqSideshow[A: Eq]: Eq[Sideshow[A]] = Eq.fromUniversalEquals
+
+  implicit def arbSideshow[A: Arbitrary]: Arbitrary[Sideshow[A]] =
+    Arbitrary(Gen.alphaStr.map(Bob(_)))
+
+  checkAll("Sideshow.Monad",MonadTests[Sideshow].monad[Int, Double, String])
+
+  test("Sideshow be a monad") {
+
+  }
+}
